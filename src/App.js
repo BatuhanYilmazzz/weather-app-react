6face52b62b6f42e8f51ec8b00d3b6b3de7191c1
Ahmet Batuhan Yılmaz <54447779+BatuhanYilmazzz@users.noreply.github.com>
2020-01-07 23:03:58 +0300

    Weather icons will be fixed

--- src/App.js
+++ src/App.js
@@ -3,54 +3,12 @@ import './App.css';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
-const API_KEY = 'a019bdabf006a82f9325c183cfe1eb84';
-
 function App() {
-  const [weather, setWeather] = useState([]);
-
-  async function fetchData(e) {
-    e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-    e.target.elements.city.value = '';
-    e.target.elements.country.value = '';
-    const data = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`
-    )
-      .then(res => res.json())
-      .then(data =>
-        setWeather({
-          city: data.name,
-          country: data.sys.country,
-          temperature: Math.round(data.main.temp - 273.15),
-          description: data.weather[0].description,
-          windSpeed: data.wind.speed,
-          cloud: data.clouds.all,
-          humidity: data.main.humidity,
-          error: ''
-        })
-      )
-      .catch(error =>
-        setWeather({
-          error: 'Please Type Correctly'
-        })
-      );
-  }
-
   return (
     <div className='App'>
       <div className='app-wrapper'>
-        <Form getWeather={fetchData} />
-        <Weather
-          city={weather.city}
-          country={weather.country}
-          temperature={weather.temperature}
-          description={weather.description}
-          windSpeed={weather.windSpeed}
-          cloud={weather.cloud}
-          humidity={weather.humidity}
-          error={weather.error}
-        />
+        <Form />
+        <Weather />
       </div>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Weather from './Weather';
 
-const Form = ({ getWeather }) => {
+const API_KEY = 'a019bdabf006a82f9325c183cfe1eb84';
+
+const Form = () => {
+  const [weather, setWeather] = useState([]);
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  async function fetchData(e) {
+    e.preventDefault();
+    setCity('');
+    setCountry('');
+    const data = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`
+    )
+      .then(res => res.json())
+      .then(data =>
+        setWeather({
+          city: data.name,
+          country: data.sys.country,
+          temperature: Math.round(data.main.temp - 273.15),
+          description: data.weather[0].description,
+          windSpeed: data.wind.speed,
+          cloud: data.clouds.all,
+          humidity: data.main.humidity,
+          error: ''
+        })
+      )
+      .catch(error =>
+        setWeather({
+          error: 'Please Type Correctly'
+        })
+      );
+  }
+
   return (
-    <form className='form' onSubmit={getWeather} autoComplete='off'>
-      <input
-        type='text'
-        name='city'
-        placeholder='Enter City'
-        className='city'
-        required
+    <React.Fragment>
+      <form className='form' onSubmit={fetchData} autoComplete='off'>
+        <input
+          type='text'
+          placeholder='City'
+          className='city'
+          required
+          value={city}
+          onChange={e => setCity(e.target.value)}
+        />
+        <input
+          value={country}
+          onChange={e => setCountry(e.target.value)}
+          type='text'
+          placeholder='Country'
+          className='country'
+          required
+        />
+        <button type='submit'>Submit</button>
+      </form>
+      <Weather
+        city={weather.city}
+        country={weather.country}
+        temperature={weather.temperature}
+        description={weather.description}
+        windSpeed={weather.windSpeed}
+        cloud={weather.cloud}
+        humidity={weather.humidity}
+        error={weather.error}
       />
-      <input
-        type='text'
-        name='country'
-        placeholder='Enter Country'
-        className='country'
-        required
-      />
-      <button type='submit'>Submit</button>
-    </form>
+    </React.Fragment>
   );
 };
 
